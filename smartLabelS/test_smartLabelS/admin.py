e40a3a10eb311eaa9541f5e0765ffbe304f1a041
als34 <als34@hi.is>
2014-03-08 12:58:06 +0000

    signup sett inn, v. 0.0.1

--- smartLabelS/test_smartLabelS/admin.py
+++ smartLabelS/test_smartLabelS/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 
 
 # Register your models here.
+from .models import SignUp
+
+class SignUpAdmin(admin.ModelAdmin):
+    class Meta:
+        model = SignUp
+        
+admin.site.register(SignUp, SignUpAdmin)
