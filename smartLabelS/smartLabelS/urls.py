6412332a2161c03e8efe2a2020244da73a1116b5
als34 <als34@hi.is>
2014-03-08 15:34:12 +0000

    signup.html

--- smartLabelS/smartLabelS/urls.py
+++ smartLabelS/smartLabelS/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'smartLabelS.views.home', name='home'),
+    url(r'^$', 'test_smartLabelS.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
