552d2c3cfe6a8f1ddf4826993ef7eaef9c78318f
kbs16 <kbs16@hi.is>
2014-03-17 15:15:42 +0000

    tók út "south"

--- smartLabelS/test_smartLabelS/views.py
+++ smartLabelS/test_smartLabelS/views.py
@@ -12,7 +12,7 @@ def home(request):
     if form.is_valid():
         save_it = form.save(commit=False)
         save_it.save()
-        messages.success(request, 'Thank you for joining, we will be in touchs')
+        messages.success(request, 'Thank you for joining, we will be in touch')
         return HttpResponseRedirect('/thank-you/')                   
     return render_to_response("signup.html", locals(), context_instance=RequestContext(request))
 
