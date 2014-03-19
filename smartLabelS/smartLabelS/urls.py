2189778ebde2dbee057dbae4ee6fcc80944d4552
kbs16 <kbs16@hi.is>
2014-03-19 15:38:19 +0000

    login bætt við. Þarf að bæta við /login til að nota það

--- smartLabelS/smartLabelS/urls.py
+++ smartLabelS/smartLabelS/urls.py
@@ -12,6 +12,11 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^thank-you/$', 'test_smartLabelS.views.thankyou', name='thankyou'),
     url(r'^learn-more/$', 'test_smartLabelS.views.learnmore', name='learnmore'),
+    url(r'^login/$', 'test_smartLabelS.views.login'),
+    url(r'^auth/$', 'test_smartLabelS.views.auth_view'),
+    url(r'^logout/$', 'test_smartLabelS.views.logout'),
+    url(r'^loggedin/$', 'test_smartLabelS.views.loggedin'),
+    url(r'^invalid/$', 'test_smartLabelS.views.invalid_login'),
     url(r'^admin/', include(admin.site.urls)),
 )
 
--- smartLabelS/test_smartLabelS/views.py
+++ smartLabelS/test_smartLabelS/views.py
@@ -3,7 +3,8 @@ from django.conf import settings
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.shortcuts import render, render_to_response, RequestContext, HttpResponseRedirect
-
+from django.contrib import auth
+from django.core.context_processors import csrf
 
 # Create your views here.
 
@@ -33,3 +34,29 @@ def thankyou(request):
 
 def learnmore(request):
     return render_to_response("learnmore.html", locals(), context_instance=RequestContext(request))
+
+def login(request):
+    c={}
+    c.update(csrf(request))
+    return render_to_response('login.html', c)
+
+def auth_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username = username, password=password)
+    
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('/loggedin')
+    else:
+        return HttpResponseRedirect('/invalid')
+    
+def loggedin(request):
+    return render_to_response('loggedin.html', {'full_name', request.user.username})
+
+def invalid_login(request):
+    return render_to_response('invalid_login.html')
+
+def logout(request):
+    auth.logout(request)
+    return render_to_response('logout.html')
